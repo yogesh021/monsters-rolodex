6157c07b8a83f163e5c9c85f1fe81c2089777a2d
yogesh021 <yogeshtripathi273@gmail.com>
2020-05-26 19:51:24 +0530

    Updates

---
+++ precache-manifest.67cd809332be6f2f218022ce6180c6fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7643119b75c36afb9afcacf1cf11b975",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1e29136c496640c269f9",
+    "url": "/monsters-rolodex/static/css/main.44aa2f5d.chunk.css"
+  },
+  {
+    "revision": "bcf2f8dc29b808a638b8",
+    "url": "/monsters-rolodex/static/js/2.af78b3c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.af78b3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e29136c496640c269f9",
+    "url": "/monsters-rolodex/static/js/main.22fb8ab6.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
